6086b41516f20d30c8b16bdef96d876293974ba9
Edi369 <joojgames369@gmail.com>
2025-08-14 17:33:12 -0300

    Troubleshooting icon

--- src/app/layout.js
+++ src/app/layout.js
@@ -18,7 +18,10 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="pt-BR">
+      <head>
+        <link rel="icon" href="/favicon.ico" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
